52176abf9a31ca681b7b16c80cc99a62258670d2
Martin Howarth <MartinHowarth@users.noreply.github.com>
2019-12-15 16:39:36 +0000

    Add doc strings to base engine widget tests

--- tests/test_engine/test_widgets/test_base.py
+++ tests/test_engine/test_widgets/test_base.py
@@ -1,3 +1,5 @@
+"""Tests for common widget elements."""
+
 import pytest
 
 from shimmer.engine.widgets.base import FloatRange
@@ -5,10 +7,12 @@ from shimmer.engine.widgets.base import FloatRange
 
 @pytest.fixture
 def dummy_float_range():
+    """Common FloatRange for use in widget tests."""
     return FloatRange(20, 100)
 
 
 def test_float_range_contains(dummy_float_range: FloatRange):
+    """Test the contains method of the FloatRange class."""
     # Check `__contains__`
     assert 12 not in dummy_float_range
     assert 50 in dummy_float_range
@@ -19,4 +23,5 @@ def test_float_range_contains(dummy_float_range: FloatRange):
 
 
 def test_float_range_len(dummy_float_range: FloatRange):
+    """Test the len method of the FloatRange class."""
     assert dummy_float_range.len() == 80
