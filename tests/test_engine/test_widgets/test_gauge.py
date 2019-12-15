5d68125b97ca693545aa03f373d43c7ce39b84d4
Martin Howarth <MartinHowarth@users.noreply.github.com>
2019-12-15 16:50:55 +0000

    Add doc strings to test_gauge.py

--- tests/test_engine/test_widgets/test_gauge.py
+++ tests/test_engine/test_widgets/test_gauge.py
@@ -1,3 +1,5 @@
+"""Tests for the gauge widget."""
+
 import pytest
 
 from shimmer.engine.widgets.gauge import GaugeDefinition, FloatRange
@@ -5,12 +7,14 @@ from shimmer.engine.widgets.gauge import GaugeDefinition, FloatRange
 
 @pytest.fixture
 def dummy_gauge_definition():
+    """Common gauge definition for use in these tests."""
     return GaugeDefinition(
         FloatRange(0, 100), FloatRange(20, 80), FloatRange(50, 75), 40,
     )
 
 
 def test_fraction_calculations(dummy_gauge_definition: GaugeDefinition):
+    """Test fractions of the gauge calculations."""
     assert dummy_gauge_definition.low_fraction_start == 0.2
     assert dummy_gauge_definition.high_fraction_start == 0.5
     assert dummy_gauge_definition.value_fraction_start == 0.4
@@ -19,6 +23,7 @@ def test_fraction_calculations(dummy_gauge_definition: GaugeDefinition):
 
 
 def test_changing_value(dummy_gauge_definition: GaugeDefinition):
+    """Test changing value of the gauge is handled correctly."""
     # Ensure value_fraction_start is updated
     assert dummy_gauge_definition.value_fraction_start == 0.4
     dummy_gauge_definition.value = 20
